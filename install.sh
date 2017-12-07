689b0aaa5afbb1d3dc52ff3891065c07574dd762
Steve N4IRS <szingman@msgstor.com>
2017-12-07 13:35:54 -0500

    Add required programs for first time install

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
+# Install the required support programs
+apt-get install unzip -y
+apt-get install python-dev -y
+apt-get install python-pip -y
+apt-get install python-twisted -y
+
 test -e ./setup.py || exit 1
-apt-get install python-dev
 pip install --upgrade .
 
