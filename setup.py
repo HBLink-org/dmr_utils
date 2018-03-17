7f5a223637662ce0113bdbb0d62c566a14751d4b
Cort Buffington <n0mjs@me.com>
2018-03-16 21:59:39 -0500

    Fix recursion problem on lesser machines

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
         return file.read()
 
 setup(name='dmr_utils',
-      version='0.1.12',
+      version='0.1.13',
       description='ETSI DMR (Digital Mobile Radio) Tier II Utilities',
       long_description='Modules to disassemble and assemble DMR packets, including generating and decoding various FEC routines',
       classifiers=[
