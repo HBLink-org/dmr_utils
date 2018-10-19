21bf6730d548ede9379acd2d7873eb96089a8ea5
Steve N4IRS <szingman@msgstor.com>
2018-10-19 10:18:32 -0400

    simplify install script

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
+# This script will install dmr_utils from a downloaded zip file or from a git clone.
+# If all you are interested in using dmr_utils because another program requires it (HBlink DMRlink etc)
+# Then you can simply:
+# apt-get install python-pip -y
+# pip install dmr_utils
 
 # Install the required support programs
-apt-get install unzip -y
-apt-get install python-dev -y
+# apt-get install unzip -y
+# apt-get install python-dev -y
 apt-get install python-pip -y
-apt-get install python-twisted -y
 
 test -e ./setup.py || exit 1
 pip install --upgrade .
